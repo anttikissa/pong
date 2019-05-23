7d5d964bded3d55adbb25eac8014cd4e772c0325
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2019-05-23 20:40:50 +0200

    Some drawing

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
-console.log('moi moduuli')
+function log(...args) {
+    console.log(...args)
+}
 
 let canvas = document.querySelector('#content')
 
+let { width, height } = canvas
+
+let ctx = canvas.getContext('2d')
+
+function update() {
+
+}
+
+function draw() {
+    log('draw')
+
+    ctx.clearRect(0, 0, width, height)
+    ctx.fillStyle = 'red'
+    ctx.fillRect(10, 10, 200, 200)
+}
+
+function loop() {
+    update()
+    draw()
+    requestAnimationFrame(loop)
+}
+
+loop()
