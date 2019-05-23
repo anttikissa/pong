37ebdc5f8eccd00cedad1cc1b974284464c7f7c9
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2019-05-24 00:59:11 +0300

    Balls properly bounce off sides, but not from corners yet

--- main.js
+++ main.js
@@ -15,6 +15,12 @@ let WORLD_HEIGHT = 9
 let SCALE = width / WORLD_WIDTH
 let ctx = canvas.getContext('2d')
 
+// math halpers
+let square = x => x * x
+let distance = (p1, p2) => Math.sqrt(square(p2.x - p1.x) + square(p2.y - p1.y))
+let distance1d = (a, b) => Math.abs(a - b)
+let between = (value, min, max) => min < value && value < max
+
 // in seconds (let's just assume this)
 let FRAME_LENGTH = 1 / 60
 
@@ -50,15 +56,48 @@ function collides(ball, box) {
 	}
 
 	// Does the ball overlap with one of the corners?
-	// let cornerOverlap = false
-	// for (let corner of box.corners) {
-	// 	if (ball.contains(corner)) {
-	// 		cornerOverlap = true
-	// 	}
-	// }
+	let cornerOverlap = false
+	for (let corner of box.corners) {
+		if (ball.contains(corner)) {
+			// TODO do something about the direction
+			cornerOverlap = true
+		}
+	}
 
-	// TODO
-	return containingBoxOverlap
+	let sideOverlap = false
+	// Top?
+	if (distance1d(ball.y, box.top) < ball.radius) {
+		if (between(ball.x, box.left, box.right)) {
+			ball.velocity.y = -Math.abs(ball.velocity.y)
+			sideOverlap = true
+		}
+	}
+
+	// Bottom?
+	if (distance1d(ball.y, box.bottom) < ball.radius) {
+		if (between(ball.x, box.left, box.right)) {
+			ball.velocity.y = Math.abs(ball.velocity.y)
+			sideOverlap = true
+		}
+	}
+
+	// Left?
+	if (distance1d(ball.x, box.left) < ball.radius) {
+		if (between(ball.y, box.top, box.bottom)) {
+			ball.velocity.x = -Math.abs(ball.velocity.x)
+			sideOverlap = true
+		}
+	}
+
+	// Right?
+	if (distance1d(ball.x, box.right) < ball.radius) {
+		if (between(ball.y, box.top, box.bottom)) {
+			ball.velocity.x = Math.abs(ball.velocity.x)
+			sideOverlap = true
+		}
+	}
+
+	return sideOverlap || cornerOverlap
 }
 
 let boxes = []
@@ -77,9 +116,34 @@ class Box {
 			},
 			y: {
 				min: this.y,
-				max: this.y + this.width
+				max: this.y + this.height
 			}
 		}
+	}
+
+	get top() {
+		return this.y
+	}
+
+	get bottom() {
+		return this.y + this.height
+	}
+
+	get left() {
+		return this.x
+	}
+
+	get right() {
+		return this.x + this.width
+	}
+
+	get corners() {
+		return [
+			{ x: this.x, y: this.y },
+			{ x: this.x + this.width, y: this.y },
+			{ x: this.x + this.width, y: this.y + this.height },
+			{ x: this.x, y: this.y + this.height }
+		]
 	}
 
 	draw() {
@@ -110,6 +174,10 @@ class Ball {
 				max: this.y + this.radius
 			}
 		}
+	}
+
+	contains(point) {
+		return distance(this, point) < this.radius
 	}
 
 	draw() {
@@ -166,11 +234,11 @@ function newBox(x, y) {
 	)
 }
 
-function newBall(x, y, velocity) {
+function newBall(x, y, velocity, radius = 0.4) {
 	if (!velocity) {
 		velocity = {
-			x: Math.random() * 20 - 10,
-			y: Math.random() * 20 - 10
+			x: Math.random() * 30 - 10,
+			y: Math.random() * 30 - 10
 		}
 	}
 
@@ -178,7 +246,7 @@ function newBall(x, y, velocity) {
 		new Ball({
 			x,
 			y,
-			radius: 0.4,
+			radius,
 			velocity
 		})
 	)
@@ -208,12 +276,19 @@ newBox(3, 1)
 newBox(4, 1)
 newBox(5, 5)
 
-newBall(6.4, 6.2, { x: -.05, y: -0.025 })
+newBall(6.4, 6.2, { x: -0.05, y: -0.025 })
+newBall(4.4, 6.0, { x: 0.5, y: 0.1 })
+newBall(4.0, 4.8, { x: 0.9, y: -0.05 })
+newBall(6.0, 4.0, { x: 0.09, y: 0.95 })
 
-// newBall(4, 4.5)
-// newBall(4, 4.5)
-// newBall(4, 4.5)
-// newBall(4, 4.5)
-// newBall(4, 4.5)
+newBall(6.4, 5.5, { x: -0.5, y: -0.025 }, 0.2)
+
+newBall(5.4, 4.5, { x: -0.0, y: 0.5 }, 0.2)
+
+newBall(4, 4.5, 0, 0.1)
+newBall(4, 4.5, 0, 0.1)
+newBall(4, 4.5, 0, 0.1)
+newBall(4, 4.5, 0, 0.1)
+newBall(4, 4.5, 0, 0.1)
 
 loop()
