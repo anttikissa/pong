59fe56a3a444a2715b7ef897fcbbc1d96ab83b7e
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2019-06-01 21:43:00 +0300

    RGB optimization (thanks Marko)

--- util.js
+++ util.js
@@ -1,16 +1,7 @@
+let first = true
 function makeFillStyle(rgb) {
-	// convert value between 0 and 1 to "00" -> "ff"
-	// if value is not between 0 and 1, you get garbage
-	function hexify(value) {
-		let n = Math.round(value * 255.0)
-		let s = n.toString(16).slice(0, 2)
-		if (s.length < 2) {
-			return '0' + s
-		}
-		return s
-	}
 	let { r, g, b } = rgb
-	return '#' + hexify(r) + hexify(g) + hexify(b)
+	return `rgb(${(r * 255) | 0}, ${(g * 255) | 0}, ${(b * 255) | 0})`
 }
 
 function log(...args) {
@@ -18,14 +9,14 @@ function log(...args) {
 }
 
 function assert(fact) {
-    if (!fact) {
-        log('Assertion is about to fail. Hope you had a debugger open!')
-        debugger
-        throw new Error('assertion failed')
-    }
+	if (!fact) {
+		log('Assertion is about to fail. Hope you had a debugger open!')
+		debugger
+		throw new Error('assertion failed')
+	}
 }
 
 function equals(a, b) {
-    // Close enough
-    return JSON.stringify(a) === JSON.stringify(b)
+	// Close enough
+	return JSON.stringify(a) === JSON.stringify(b)
 }
